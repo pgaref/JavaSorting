7c5d0378b91734a7b38b382947cce2d57606b076
PG <PG@Home-Desktop>
2014-03-01 15:37:57 +0200

    Fixing Build path

--- src/java/main/Project1.java
+++ src/java/main/Project1.java
@@ -1,16 +1,35 @@
 
 public class Project1 {
-	
-	
-	
-	
-	
-	
-	public static void name (String [] args) {
-		
-		
-		
-		
+	public static void insertion_srt(int array[], int n) {
+		for (int i = 1; i < n; i++) {
+			int j = i;
+			int B = array[i];
+			while ((j > 0) && (array[j - 1] > B)) {
+				array[j] = array[j - 1];
+				j--;
+			}
+			array[j] = B;
+		}
 	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		int i;
+		int array[] = { 12, 9, 4, 99, 120, 1, 3, 10 };
+		System.out.println("\n\n RoseIndia\n\n");
+		System.out.println(" Selection Sort\n\n");
+		System.out.println("Values Before the sort:\n");
+		for (i = 0; i < array.length; i++)
+			System.out.print(array[i] + "  ");
+		System.out.println();
+		insertion_srt(array, array.length);
+		System.out.print("Values after the sort:\n");
+		for (i = 0; i < array.length; i++)
+			System.out.print(array[i] + "  ");
+		System.out.println();
+		System.out.println("PAUSE");
 
+	}
 }
