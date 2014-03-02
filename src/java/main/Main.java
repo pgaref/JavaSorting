9d7a9e402d104f0af48617ac9c948320fedd7adc
pgaref <pgaref@macbook-air.lan>
2014-03-02 02:28:11 +0200

    Almost Done

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -3,51 +3,81 @@ import java.util.ArrayList;
 
 public class Main {
 
-	
+	public static void usage(){
+		System.out.println("Usage: ");
+		System.out.println("\tjava -jar sorting.jar <input.txt> <output.txt> <O|I> <N|S|Y|M|D>");
+		
+	}
 	public static void main(String[] args) {
+		String sortingAlg = null;
+		String infile;
+		String outfile;
 		
-		
-		/* Sorting arguments */
 		ArrayList<String> arguments = new ArrayList<String>();
-		arguments.add("Y");
-		arguments.add("M");
-		arguments.add("D");
+		ArrayList<String> check = new ArrayList<String>();
+		check.add("I");
+		check.add("O");
+		
+		/* Arguments parser */
+		 
+		if(args.length !=4){
+			System.out.println("Wrong arguments nnumber!");
+			usage();
+			System.exit(-1);
+		}
+		infile = args[0];
+		outfile = args[1];
+		if(!check.contains(args[2])){
+			System.out.println("Wrong argument "+ args[2]);
+			usage();
+			System.exit(-1);
+		}
+		else
+			sortingAlg = args[2];
+		check.clear();
+		check.add("N");
+		check.add("S");
+		check.add("Y");
+		check.add("M");
+		check.add("D");
+		
+		for(int i = 0 ; i < args[3].length(); i++){
+			if(!check.contains(args[3].charAt(i)+"")){
+				System.out.println("Wrong argument "+ args[3].charAt(i));
+				usage();
+				System.exit(-1);
+			}
+			else
+				arguments.add(args[3].charAt(i)+"");
+		}
+		
+		//System.out.println("In: "+ infile +" Out: "+ outfile +" Sort: "+ sortingAlg + " Arguments: " + arguments.toString());
 		
 		Person.SortArguments = arguments;
 		
-		/* The End */
-		
 		FileHandler fh = new FileHandler();
-		ArrayList<Person> list = fh.ParseFile("test.txt");
+		ArrayList<Person> list = fh.ParseFile(infile);
 		
-		System.out.println("----- Before ------");
+		System.out.println("----- File Before ------");
 		for(Person p : list){
 			System.out.println(p);
 		}
 		
-		InsertionSort.insertion_srt(list);
+		if(sortingAlg == "I")
+			InsertionSort.insertion_srt(list);
+		else if(sortingAlg  == "O")
+			BubbleSort.bubblesrt(list);
 		
-		System.out.println("----- After ------");
+		
+		System.out.println("----- File After ------");
 		for(Person p : list){
 			System.out.println(p);
 		}
 		
+		fh.WriteToFile(outfile, list);
+		System.out.println("All done!");
 		
-		/*int i;
-		int array[] = { 12, 9, 4, 99, 120, 1, 3, 10 };
-		System.out.println("\n\n RoseIndia\n\n");
-		System.out.println(" Selection Sort\n\n");
-		System.out.println("Values Before the sort:\n");
-		for (i = 0; i < array.length; i++)
-			System.out.print(array[i] + "  ");
-		System.out.println();
-		InsertionSort.insertion_srt(array, array.length);
-		System.out.print("Values after the sort:\n");
-		for (i = 0; i < array.length; i++)
-			System.out.print(array[i] + "  ");
-		System.out.println();
-		System.out.println("PAUSE");
-		 */
+		
 		
 		
 		
